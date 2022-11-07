074d40b527883672e63c5c79f3f3ab0e45d99171
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2022-11-07 12:36:17 +1030

    update2

---
+++ ExampleREADME.md
@@ -0,0 +1,39 @@
+# Professional-README-Generator
+
+  ## Description
+
+  
+  test
+
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#license)
+  * [Contributing](#Contributing)
+  * [Tests](#Tests)
+  * [Questions](#Questions)
+  
+  ***
+
+  ## Installation
+
+  test
+
+  ## Usage
+
+  test
+
+  undefined
+
+  ## Contributing
+
+  test
+
+  ## Tests
+
+  test
+  
+  ## Questions
+  >Email: Zachary.Donovan88@outlook.com 
+  >GitHub : [undefined](https://github.com/undefined)
+  
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // TODO: Include packages needed for this application
-const packageJ = require('../Professional-README-Generator/package.json');
 const fs = require('fs');
-const util = require('../Professional-README-Generator/utils/generateMarkdown');
-const { default: inquirer } = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
 
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
+        name:'githubuser',
         message: "what is your Github user name?",
         default: 'Github user name',
         validate: function (answer) {
@@ -21,6 +21,7 @@ const questions = [
 
     {
         type: 'input',
+        name:'githubrepo',
         message: "what is your Github repo?",
         default: 'Github repo',
         validate: function (answer) {
@@ -33,6 +34,7 @@ const questions = [
 
     {
         type: 'input',
+        name:'title',
         message: "What is the title of your project?",
         default: 'project title',
         validate: function (answer) {
@@ -72,21 +74,18 @@ const questions = [
         choices: ['MIT', 'ISC', 'GNU LGPL', 'Unlicense', 'none'],
         default: 'MIT'
     }
-    .then(input => {
-        return input;
-    })
-];
+   ];
 
-const writeFileAsync = util.promisify(writeToFile);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile, data, err => {
-        return console.log(err);
-    }
+    fs.writeFileSync(fileName, data,(err,res) => {
+        if(err) console.log(err);
+        console.log("Congratulations! Your README.md file was successful and has been generated!")
+    })
 
-    console.log("Congratulations! Your README.md file was successful and has been generated!")
 }
+
 
 // TODO: Create a function to initialize app
 async function init() {
@@ -94,15 +93,13 @@ async function init() {
         const userResponses = await inquirer.prompt(questions);
         console.log("Your responses: ", userResponses);
         console.log("Thank you for your responses! Fetching your GitHub data next...");
-    
-        const userInfo = await api.getUser(userResponses);
-        console.log("Your GitHub user info: ", userInfo);
+
     
         console.log("Generating your README next...")
-        const markdown = generateMarkdown(userResponses, userInfo);
+        const markdown = generateMarkdown(userResponses);
         console.log(markdown);
     
-        await writeFileAsync('ExampleREADME.md', markdown);
+        await writeToFile('ExampleREADME.md', markdown);
 
     } catch (error) {
         console.log(error);
